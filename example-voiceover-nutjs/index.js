9ff5fb07b8b43fa3b1617ec5832d2b21a47d7c53
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2021-04-11 13:26:30 +0200

    simple voiceover example

---
+++ example-voiceover-nutjs/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const { keyboard, Key, mouse, left, right, up, down, screen } = require("@nut-tree/nut-js");
+const open = require('open');
+
+(async () => {
+    await open('https://www.a11yproject.com/', {app: {name: open.apps.chrome}});
+    
+    // try to activate voice over (mac osx)
+    await keyboard.type(Key.SuperLeft, Key.F5);
+})();
