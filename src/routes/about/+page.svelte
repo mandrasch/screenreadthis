1ac299e4e9002781be5f7eccbcb1a9259941564c
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-09-03 00:08:56 +0200

    update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,11 +8,17 @@
 
 	<p>
 		The goal of this project is to enable an easier way of (quickly) testing the screenreader output
-		of websites. Basic web accessibility testing should be possible without having to install
-		screenreader software and the need learn the different keyboard shortcuts.
+		of websites.
+		<br />
+		Basic web accessibility testing should be possible without having to install screenreader software
+		and without the need of learning the different keyboard shortcuts first.
+	</p>
+	<p>
+		Having said that, serious and professional accessibility testing should be always done with real
+		screenreader software.
 	</p>
 
-	<h2>General</h2>
+	<h2>Technical information</h2>
 	<p>
 		This web app loads the <a
 			href="https://developer.mozilla.org/en-US/docs/Glossary/Accessibility_tree"
@@ -28,23 +34,32 @@
 		output is realized via
 		<a href="https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis">WebSpeech API.</a>
 	</p>
-	<p>The source code for the frontend can be found here:<a href="https://github.com/mandrasch/screenread-this">mandrasch/screenread-this</a></p>
+	<p>
+		Frontend source code: <a href="https://github.com/mandrasch/screenread-this"
+			>mandrasch/screenread-this</a
+		>
+	</p>
 
 	<h2>Backend (API)</h2>
 	<p>
 		The Accessibility Tree data is retrieved via JSON from a NodeJS server running
 		<a href="https://pptr.dev/">Puppeteer</a>. Puppeteer runs headless and is a NodeJS library which
-		provides a high-level API to control Chrome or Chromium over the DevTools Protocol. The source
-		for the backend API server can be found here:
-		<a href="https://github.com/mandrasch/a11y-tree-api">mandrasch/a11y-tree-api</a>.
+		provides a high-level API to control Chrome or Chromium over the DevTools Protocol.
+	</p>
+	<p>
+		Backend source code: <a href="https://github.com/mandrasch/a11y-tree-api"
+			>mandrasch/a11y-tree-api</a
+		>
 	</p>
 
 	<h2>Limitations</h2>
 	<p>
-		Currently only the english language is supported. But the WebSpeech API has support for multiple languages, the language could be detected via "html lang"-attribute.
+		Currently only the english language is supported. But the WebSpeech API has support for multiple
+		languages, the language could be detected via "html lang"-attribute.
 	</p>
 	<p>
-		There is no support for actually interacting with the page. This would require keeping the connection to Puppeteers browser instance (session). Happy to hear your suggestions for
+		There is no support for actually interacting with the page. This would require keeping the
+		connection to Puppeteers browser instance (session). Happy to hear your suggestions for
 		implementing this!
 	</p>
 
